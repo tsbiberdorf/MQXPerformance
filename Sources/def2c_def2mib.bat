f366bcee88c5797eb0ff5362b990f322d6c39985
Terry Biberdorf <terry.biberdorf@symcom.com>
2013-07-24 10:58:04 -0600

    basic snmp v2c project with ADC sample task running

---
+++ Sources/def2c_def2mib.bat
@@ -0,0 +1,3 @@
+..\..\..\Freescale_MQX_4_0\tools\gawk.exe -f ..\..\..\Freescale_MQX_4_0\tools\snmp\def2c.awk snmp_demo.def > snmp_demo.c
+..\..\..\Freescale_MQX_4_0\tools\gawk.exe -f ..\..\..\Freescale_MQX_4_0\tools\snmp\def2mib.awk snmp_demo.def > snmp_demo.mib
+pause
---
+++ Sources/main.h
@@ -0,0 +1,50 @@
+#ifndef __main_h_
+#define __main_h_
+#include <mqx.h>
+#include <bsp.h>
+#include <shell.h>
+#define APPLICATION_HAS_SHELL
+
+#include <mfs.h>
+
+
+
+#include <rtcs.h>
+#ifdef APPLICATION_HAS_SHELL
+#include <sh_rtcs.h>
+#endif
+#include <ipcfg.h>
+
+
+#define MAIN_TASK 1
+
+
+extern void Main_task(uint_32);
+
+
+/* PPP device must be set manually and 
+** must be different from the default IO channel (BSP_DEFAULT_IO_CHANNEL) 
+*/
+#define PPP_DEVICE      "ittyb:" 
+
+/*
+** Define PPP_DEVICE_DUN only when using PPP to communicate
+** to Win9x Dial-Up Networking over a null-modem
+** This is ignored if PPP_DEVICE is not #define'd
+*/
+#define PPP_DEVICE_DUN  1
+
+/*
+** Define the local and remote IP addresses for the PPP link
+** These are ignored if PPP_DEVICE is not #define'd
+*/
+#define PPP_LOCADDR     IPADDR(192,168,0,216)
+#define PPP_PEERADDR    IPADDR(192,168,0,217)
+
+/*
+** Define a default gateway
+*/
+#define GATE_ADDR       IPADDR(192,168,0,1)
+
+#endif /* __main_h_ */
+
